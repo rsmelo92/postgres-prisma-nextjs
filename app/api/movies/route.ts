b7a78252b47dd897703815a54fdb8723a0810c24
Rafael Melo <rsmelo_@hotmail.com>
2024-02-11 23:01:06 -0300

    fix: correct pagination

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -7,9 +7,19 @@ export const dynamic = 'force-dynamic'; // static by default, unless reading the
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const cursor = searchParams.get('cursor') ?? "";
+  if(cursor === "") {
+    const result = await prisma.movie.findMany({
+      take: 10,
+      orderBy: {
+        id: "asc"
+      },
+    })
+    return new Response(JSON.stringify(result));
+  }
+
   const result = await prisma.movie.findMany({
     take: 10,
-    skip: 0,
+    skip: 1,
     orderBy: {
       id: "asc"
     },
